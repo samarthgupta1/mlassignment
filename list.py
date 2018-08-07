185f327cb201f4c62afe772d022c914d3c7ec96b
samarthgupta1 <samarth1250@gmail.com>
2018-08-08 01:18:46 +0530

    list

---
+++ list.py
@@ -0,0 +1,13 @@
+x=[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]
+#QUESTION1
+print "QUESTION1"
+print x[7:12:1]
+print x[-9:-4:1]
+
+#QUESTION1
+print "QUESTION2"
+print x[1::2]
+
+#QUESTION1
+print "QUESTION2"
+print x[::4]
